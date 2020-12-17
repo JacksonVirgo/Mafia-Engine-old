1da84e1c5c13358fef6ccc2caf4848540ff1d6e3
JacksonVirgo <jacknash42@gmail.com>
2020-12-17 20:47:08 +1000

    Singular Role Format

--- client/role_card_generator/role_card.js
+++ client/role_card_generator/role_card.js
@@ -1,41 +1,17 @@
 SINGULAR_ROLE = "#singular-role";
 SINGULAR_RESULT = "#singular-result";
 SINGULAR_RESULT_COPY = "#singular-result-copy";
-SINGULAR_RESULT_SEND = "#singular-result-send";
 ROLE_COLOUR = "#rcolour";
+ADD_FIELD = "#add-field";
+GENERATE_BTN = "#generate-btn";
 
-String.prototype.replaceAt = function(index, replacement) {
-    if (index >= this.length) {
-        return this.valueOf();
-    }
-    return this.substring(0, index) + replacement + this.substring(index + 1);
-}
-
-var URLCleaned = {
-    "!": "%21",
-    "#": "%23",
-    "$": "%24",
-    "%": "%25",
-    "&": "%26",	
-    "'": "%27",
-    "("	: "%28",
-    ")"	: "%29",
-    "*"	: "%2A",
-    "+"	: "%2B",
-    ","	: "%2C",
-    "/"	: "%2F",
-    ":"	: "%3A",
-    ";"	: "%3B",
-    "="	: "%3D",
-    "?"	: "%3F",
-    "@": "%40",
-    "[": "%5B",
-    "]": "%5D",
-}
+// Send as PM Form
+SEND_AS_PM = "#send-as-pm";
 
 $(document).ready(() => {
     console.log("JQuery Initialized");
 
+    var currentCustoms = 0;
     $(SINGULAR_ROLE).on('submit', (e) => {
         e.preventDefault();
         console.log("Singular Role Submitted");
@@ -46,20 +22,53 @@ $(document).ready(() => {
             array[field.name] = field.value;
         });
 
+        console.log(array);
+
         var role = generateFormattedRole(array);
         $(SINGULAR_RESULT).text(`${role}`);
     });
+    $(ADD_FIELD).click(() => {
+        var label = $("<label/>")
+        var component = $("<input/>");
 
-    $(SINGULAR_RESULT_COPY).click(() => {
-        //clipboardCopy($(SINGULAR_RESULT).text());
-        clipboardCopy(generateForumPM("MafiaScum", "Role PM", $(SINGULAR_RESULT).text()));
-    });    
-    $(SINGULAR_RESULT_SEND).click(() => {
-        var link = generateForumPM("Town of Salem", "Role PM", $(SINGULAR_RESULT).text());
-        window.location.align = link;
+        var id = `custom${currentCustoms}`;
+        component.attr("id", id);
+        component.attr("name", id);
+        label.attr("for", id);
+        label.text(id);
+
+        currentCustoms += 1;
+
+        // Move the role template down
+        $(SINGULAR_ROLE).append(label).append(component).append($("#template-lbl")).append($("#role_template")).append($(GENERATE_BTN));
     });
 
+    $(SINGULAR_RESULT_COPY).click(() => {
+        clipboardCopy($(SINGULAR_RESULT).text());
+    });
+    // $(SINGULAR_RESULT_SEND).click(() => {
+    //     var link = generateForumPM("Town of Salem", "Role PM", $(SINGULAR_RESULT).text());
+    //     window.location.align = link;
+    // });
+
+    $(SEND_AS_PM).on('submit', (e) => {
+        e.preventDefault();
+        var results = serialize(SEND_AS_PM);
+        var array = {};
+        $.each(results, (i, field) => {
+            array[field.name] = field.value;
+        });
+
+        var link = generateForumPM(array.forum, "Role PM", $(SINGULAR_RESULT).val());
+        console.log(link);
+        openInNewTab(link);
+    });
 });
+
+function openInNewTab(url) {
+    var win = window.open(url, '_blank');
+    win.focus();
+}
 
 function clipboardCopy(text) {
     var tmp = document.createElement("textarea");
@@ -74,7 +83,7 @@ function generateFormattedRole(roleArray) {
 
     var roleArrayTmp = roleArray;
     console.log("Gen Format");
-    var template = $("#role_template").text();
+    var template = $("#role_template").val();
     var namesArray = [];
     for (var property in roleArrayTmp) {
         namesArray.push(property);
@@ -92,29 +101,16 @@ function generateFormattedRole(roleArray) {
 
 function generateForumPM(website, subject, message) {
     var link = "";
-    var cleanedSubject = encodeURI(subject);
-    var cleanedMessage = encodeURI(message);
-    if (website === "Town of Salem") {
-        link = `https://www.blankmediagames.com/phpbb/ucp.php?i=pm&mode=compose&username_List=JacksonVirgo&subject=${cleanedSubject}&message=${cleanedMessage}`;
-    } else if (website === "MafiaScum") {
-        link = `https://forum.mafiascum.net/ucp.php?i=pm&mode=compose&username_list=JacksonVirgo&subject=${cleanedSubject}&message=${cleanedMessage}`;
+    var cleanedRecipient = encodeURIComponent("JacksonVirgo");
+    var cleanedSubject = encodeURIComponent(subject);
+    var cleanedMessage = encodeURIComponent(message);
+    if (website === "tos") {
+        link = `https://www.blankmediagames.com/phpbb/ucp.php?i=pm&mode=compose&username_list=${cleanedRecipient}&subject=${cleanedSubject}&message=${cleanedMessage}`;
+    } else if (website === "ms") {
+        link = `https://forum.mafiascum.net/ucp.php?i=pm&mode=compose&username_list=${cleanedRecipient}&subject=${cleanedSubject}&message=${cleanedMessage}`;
     }
     return link;
 }
-
-function cleanURL(text) {
-    var cleanedText = text;
-    var blacklist = "!*'();:@&=+$,/?#[]";
-    var testArray = Array.from(cleanedText);
-    for (var i = 0; i < testArray.length; i++) {
-        if ((blacklist.includes(testArray[i]))) {
-            cleanedText = cleanedText.replaceAt(testArray[i], `${URLCleaned[testArray[i]]}`);
-        }
-    }
-    return cleanedText;
-}
-
-
 
 function serialize(sel) {
     var arr,
