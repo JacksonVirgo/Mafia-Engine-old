c9a6452e7f909c0d37014e40fa96106c75778a7d
JacksonVirgo <jacknash42@gmail.com>
2021-03-01 01:59:56 +1000

    FIX VoteCountForm Reference

--- frontend/src/pages/VoteCount.js
+++ frontend/src/pages/VoteCount.js
@@ -3,7 +3,7 @@ import '../css/reset.css';
 import '../css/app.css';
 import VoteCount from '../components/VoteCountForm';
 import { CenterModal } from '../components/centerModal';
-export default function Replacement() {
+export default function VoteCountForm() {
     return (
         <CenterModal title='Vote Count' child={(<VoteCount />)} />
     )
