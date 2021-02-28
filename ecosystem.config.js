8efa78048edb98bb27f0595ef992b00221430244
JacksonVirgo <jacknash42@gmail.com>
2021-03-01 01:30:52 +1000

    FIX PM2 Installation

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [{
+        name: "index",
+        script: 'index.js',
+        instances: 'max',
+        env: {
+            NODE_ENV: "development"
+        },
+        env_production: {
+            NODE_ENV: "production"
+        }
+    }]
+};
