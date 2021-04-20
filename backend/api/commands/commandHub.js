5d83be3411115b79b8718d027c50c474dca8ae62
Jack Nash <jacknash42@gmail.com>
2021-04-20 20:00:56 +1000

    ADD Ping Command

--- backend/api/commands/commandHub.js
+++ backend/api/commands/commandHub.js
@@ -3,6 +3,7 @@ const replacement = require('./replacement');
 const rolecard = require('./rolecard');
 module.exports = {
 	addCommands: (socket) => {
+		socket.on('ping', (data) => socket.emit('ping', data));
 		socket.on('votecount', (data) => voteCount(socket, data));
 		socket.on('replacement', (data) => replacement(socket, data));
 		socket.on('parseCSV', (data) => rolecard.parseCSV(socket, data));
